1fe1f9f961af74f94631bf4bcd5e98ed0fe2d8d8
Pawan Kumar <pawan41281@gmail.com>
2025-03-18 14:08:57 +0530

    initial commit

---
+++ src/main/java/com/example/kafkaconsumerapp/KafkaconsumerappApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaconsumerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaconsumerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaconsumerappApplication.class, args);
+	}
+
+}
