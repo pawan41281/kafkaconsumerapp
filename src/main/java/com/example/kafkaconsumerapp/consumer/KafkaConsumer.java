8f265018275ca9d3ad7413be2a150fa7a732b27f
pawan <pawan41281@gmail.com>
2025-03-19 17:01:21 +0530

    json serializer done

--- src/main/java/com/example/kafkaconsumerapp/consumer/KafkaConsumer.java
+++ src/main/java/com/example/kafkaconsumerapp/consumer/KafkaConsumer.java
@@ -3,12 +3,14 @@ package com.example.kafkaconsumerapp.consumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import com.example.vo.TransactionVo;
+
 @Component
 public class KafkaConsumer {
 
-	@KafkaListener(topics = "my-topic", groupId = "consumergroup1")
-	public void consumeMessage(String message) {
-		System.out.println("consumer1|subscribed message|" + message);
+	@KafkaListener(topics = "my-topic2", groupId = "consumergroup2")
+	public void subscribeTransactionMessage(TransactionVo transactionVo) {
+		System.out.println("consumer1|subscribe Transaction Message|" + transactionVo);
 	}
 	
 }
