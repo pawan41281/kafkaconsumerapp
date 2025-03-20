7c913d9aabd2af781efffa70bfbc452e16f2f5c1
pawan <pawan41281@gmail.com>
2025-03-20 09:56:36 +0530

    code update

--- src/main/java/com/example/kafkaconsumerapp/config/KafkaConsumerConfig.java
+++ src/main/java/com/example/kafkaconsumerapp/config/KafkaConsumerConfig.java
@@ -1,39 +1,42 @@
 package com.example.kafkaconsumerapp.config;
 
-import java.util.HashMap;
-import java.util.Map;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import org.apache.kafka.clients.consumer.ConsumerConfig;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+//import org.springframework.kafka.config.KafkaListenerContainerFactory;
+//import org.springframework.kafka.core.ConsumerFactory;
+//import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+//import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+//import org.springframework.kafka.support.serializer.JsonDeserializer;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-
-@Configuration
+//@Configuration
 public class KafkaConsumerConfig {
 
-	@Bean
-	public ConsumerFactory<String, Object> consumerFactory(){
-		Map<String, Object> map = new HashMap<>();
-		map.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.29.109:9092");
-		map.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringDeserializer.class);
-		map.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonDeserializer.class);
-		map.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.example.vo.TransactionVo");
-		map.put(ConsumerConfig.GROUP_ID_CONFIG, "my-topic2");
-		map.put(JsonDeserializer.TRUSTED_PACKAGES, "com.example.vo");
-		return new DefaultKafkaConsumerFactory<>(map);
-	}
-	
-	@Bean
-	public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Object>> kafkaListenerContainerFactory(){
-		ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-		return factory;
-	}
+//	@Bean
+//	public ConsumerFactory<String, Object> consumerFactory(){
+//		Map<String, Object> map = new HashMap<>();
+//		
+//		map.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.29.109:9092");
+//		map.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringDeserializer.class);
+//		map.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonDeserializer.class);
+//		map.put(ConsumerConfig.GROUP_ID_CONFIG, "transactions_group");
+//		
+//		map.put(JsonDeserializer.TRUSTED_PACKAGES, "com.example.vo");
+//		map.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.example.vo.TransactionVo");
+//		
+//		return new DefaultKafkaConsumerFactory<>(map);
+//	}
+//	
+//	@Bean
+//	public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Object>> kafkaListenerContainerFactory(){
+//		ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(consumerFactory());
+//		return factory;
+//	}
 	
 	
 	
--- src/main/java/com/example/kafkaconsumerapp/consumer/KafkaConsumer.java
+++ src/main/java/com/example/kafkaconsumerapp/consumer/KafkaConsumer.java
@@ -1,16 +1,36 @@
 package com.example.kafkaconsumerapp.consumer;
 
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 import com.example.vo.TransactionVo;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class KafkaConsumer {
 
-	@KafkaListener(topics = "my-topic2", groupId = "consumergroup2")
+	// @RetryableTopic is used for Dead Letter Topics or Dead Letter Queue
+	@RetryableTopic(attempts = "3", backoff = @Backoff(delay = 2000, multiplier = 2))
+	@KafkaListener(topics = "transactions", groupId = "transactions_group")
 	public void subscribeTransactionMessage(TransactionVo transactionVo) {
-		System.out.println("consumer1|subscribe Transaction Message|" + transactionVo);
+		log.info("Received: {}", transactionVo);
+		if (transactionVo.getAmount() > 0) {
+			System.out.println("consumer1|subscribe Transaction Message|" + transactionVo);
+		} else {
+			throw new RuntimeException("Transaction amount is not valid!!");
+		}
 	}
-	
+
+	@DltHandler
+	public void handleDLT(TransactionVo transactionVo) {
+		log.info("DLT : {}",transactionVo);
+	}
+
 }
